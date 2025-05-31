0e745720ae5cbfcd3be76e020ef16e7fca84a95c
Carlos Blanco <blanco.s.carlos06@gmail.com>
2025-05-30 20:44:52 -0400

    Capacidad de crear posts con compose && Darle estilo a los post

--- app.js
+++ app.js
@@ -5,13 +5,24 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
 })
 
+let post_list = []
+
 app.get("/blog", (req, res) => {
-    res.render("blog.ejs")
+    res.render("blog.ejs", {post_list})
+})
+
+// create post and post_list
+
+app.post("/submit", (req, res) => {
+    console.log(req.body)
+    post_list.push(req.body)
+    res.redirect("/blog")
 })
 
 app.listen(port, () => {
