7e670bd7fd2d5964c520e3b70d10add7b5311897
Carlos Blanco <blanco.s.carlos06@gmail.com>
2025-06-06 14:44:45 -0400

    Agregar funcionalidad al boton 'DarkMode'

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,7 @@ document.getElementById("compose").addEventListener("click", () => {
 document.getElementById("close-compose").addEventListener("click", () => {
     document.getElementById("compose-screen").classList.remove("show-compose");
 })
+
+document.getElementById("light_dark_mode").addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+})
