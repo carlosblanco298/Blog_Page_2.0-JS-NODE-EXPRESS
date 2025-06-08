603fbc271f81b18f790f99194447d8bbd46d3832
Carlos Blanco <blanco.s.carlos06@gmail.com>
2025-06-08 09:30:12 -0400

    Add edit and delete post button and delete functionality

--- app.js
+++ app.js
@@ -19,9 +19,15 @@ app.get("/blog", (req, res) => {
 
 // create post and post_list
 
+let idGen = 0
+
 app.post("/submit", (req, res) => {
-    console.log(req.body)
-    post_list.push(req.body)
+    post_list.push({id: idGen++, title: req.body.title, description: req.body.description, content: req.body.content})
+    res.redirect("/blog")
+})
+
+app.post("/delete", (req, res) => {
+    post_list = post_list.filter((post) => post.id != req.body.id)
     res.redirect("/blog")
 })
 
